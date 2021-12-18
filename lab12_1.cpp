8a7867cb8d8b3dd2f79b70ba9af75dfe22fab4b1
tahhh <thadpongsermsook1@gmail.com>
2021-12-18 14:43:05 +0700

    Lab12_1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,39 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get ";
+
+    srand(time(0));
+    int x = rand()%9;
+    if(x == 0)
+        cout << "A";
+    else if(x == 1)
+        cout << "B+";
+    else if(x == 2)
+        cout << "B";
+    else if(x == 3)
+        cout << "C+";
+    else if(x == 4)
+        cout << "C";
+    else if(x == 5)
+        cout << "D+";
+    else if(x == 6)
+        cout << "D";
+    else if(x == 7)
+        cout << "F";
+    else if(x == 8)
+        cout << "W";
+    
+    cout << " in this 261102.";
+    return 0;
+}
+
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
